1ecc50b57b69844e0d8c473ee3e5ff86e924793e
L2HYUNN <dhl9810@naver.com>
2022-03-10 19:41:29 +0900

    Week2 assignment change last

--- Assignment/assignment1.cpp
+++ Assignment/assignment1.cpp
@@ -5,11 +5,12 @@ const float PI = 3.141592;
 int main()
 {
   float dt, T;
-  T = 10. / 220.;
+  T = 5. / 220.;
   dt = 1. / 220. / 5.;
 
-  ofstream out("assignment1.txt");
+  ofstream out("assignment1-4.txt");
   for (float t = 0.0; t < T; t += dt)
+
   {
     out << exp(110 * t) * cos(220 * PI * t) << " " << exp(110 * t) * sin(220 * PI * t) << endl;
   }
