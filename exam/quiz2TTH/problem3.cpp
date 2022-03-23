44ddc526aa2d770e702e38eb4f37e7e8ef257082
L2HYUNN <dhl9810@naver.com>
2022-03-23 19:06:00 +0900

    Exam / quiz2TTH / problem3

---
+++ exam/quiz2TTH/problem3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i, ii[3];
+  short *ss;
+  for (i = 0; i < 3; i++)
+  {
+    ii[i] = (5 + i) * 0x10000 + 15 + i;
+  }
+  ss = (short *)(ii + 1);
+  for (i = -1; i < 2; i++)
+  {
+    ss[i] = i * 2 + 5;
+  }
+  for (i = 0; i < 3; i++)
+  {
+    cout << hex << ii[i] << endl;
+  }
+  return 0;
+}
