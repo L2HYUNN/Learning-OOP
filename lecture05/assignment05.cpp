09864781f1f9de28aef88e87e2c2c84d543bb1dd
L2HYUNN <dhl9810@naver.com>
2022-03-29 18:40:49 +0900

    something change

--- lecture05/assignment05.cpp
+++ lecture05/assignment05.cpp
@@ -1,36 +1,20 @@
 #include <iostream>
 #include <fstream>
-#include <cmath>
 using namespace std;
 
 int main()
 {
-  char header[44];
-
   ifstream myfile("example.wav", ios::in | ios::binary);
+  char header[44];
   myfile.read(header, sizeof(header));
-
-  // int *header2;
-  // header2 = (int *)header;
-
-  // for (int i = 0; i < 11; i++)
-  // {
-  //   cout << hex << header2[i] << endl;
-  // }
 
   unsigned int *SampleRate;
   SampleRate = (unsigned int *)(header + 24);
 
-  // cout << "SampleRate: " << SampleRate[0] << endl;
-
   myfile.seekg(SampleRate[0] * 10);
+
   short data[1000];
   myfile.read((char *)data, 1000);
-
-  for (int i = 0; i < 1000; i++)
-  {
-    cout << data[i] << endl;
-  }
   myfile.close();
 
   ofstream myfile2("data.txt", ios::out | ios::binary);
@@ -39,11 +23,7 @@ int main()
   {
     myfile2 << i * dt << " " << data[i * 2] << " " << data[i * 2 + 1] << endl;
   }
+  myfile2.close();
 
   return 0;
 }
-
-// int *chunkID;
-// chunkID = (int *)header;
-
-// cout << chunkID[2] << endl;
