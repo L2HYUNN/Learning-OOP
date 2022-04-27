e5729d8d49a2f349d2520751745ed183415e118b
L2HYUNN <dhl9810@naver.com>
2022-04-27 20:05:53 +0900

    midterm exam problem4

---
+++ exam/midterm_exam/problem4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  float f1, f2;
+  for (float x = 0.0; x < 10.0; x += 0.1)
+  {
+    f1 = 2 * x;
+    f2 = x * x;
+    if (f1 == f2)
+    {
+      cout << "yes you all right" << endl;
+    }
+    if (f1 < f2)
+    {
+      cout << x << endl;
+      break;
+    }
+  }
+  return 0;
+}
